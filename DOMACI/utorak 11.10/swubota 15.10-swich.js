ba8237443f6bdf304ed85b6b4dd3b1d11b636761
DraganaJelic985 <dragana.jelic985@gmail.com>
2022-10-16 20:22:52 +0200

    Conditions 08.10

---
+++ DOMACI/utorak 11.10/swubota 15.10-swich.js
@@ -0,0 +1,29 @@
+var a=12;
+var result='';
+
+switch (a)
+{   
+case 1:
+        result="ponedeljak";
+        break;
+case 2:
+        result="utorak";
+            break;
+case 3:
+        result="sreda";
+                break;
+case 4:
+        result="cetvrtak";
+                    break;
+case 5:
+        result="petak"
+                        break;
+case 6:
+        result="subota"
+                            break;   
+case 7:
+    resullt="nedelja"
+default:   
+result="uneti broj mora biti izmedju 1 i 7" };  
+
+console.log (result);
---
+++ DOMACI/utorak 11.10/swubota 15.10-swich2 zad.js
@@ -0,0 +1,19 @@
+
+var a=1;
+var result='';
+
+switch(a){
+case 1:
+case 2:
+case 3:
+case 4:
+case 5:
+    result='it\'s weekday';
+    break;
+case 6:
+case 7:
+result='it\'s a weekend'
+default:   
+result="uneti broj mora biti izmedju 1 i 7" };  
+
+console.log(result)
